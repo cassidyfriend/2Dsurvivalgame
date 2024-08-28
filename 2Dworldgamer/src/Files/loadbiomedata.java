9bf04f60b424581b2b691289d75737a56443e3b5
cassidyfriend <113076481+cassidyfriend@users.noreply.github.com>
2024-08-27 21:42:16 -0700

    update

--- 2Dworldgamer/src/Files/loadbiomedata.java
+++ 2Dworldgamer/src/Files/loadbiomedata.java
@@ -1,6 +1,7 @@
 package Files;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.json.*;
@@ -9,8 +10,42 @@ public class loadbiomedata {
 	
 	public static String tempsorder[][];
 	public static  JSONObject biomedata;
-	public static int maxheight = 0;
-	public static int minheight = 0;
+	
+	public class biomesindemtypes{
+		ArrayList<temperatures> types = new ArrayList<temperatures>();
+		String name;
+		public biomesindemtypes(String name){
+			this.name = name;
+		}
+		public int hashCode(){
+			return name.hashCode();
+		}
+		public boolean equals(Object o){
+			if(o.hashCode() == this.hashCode()){
+				return true;
+			}
+			return false;
+		}
+	}
+	
+	public class temperatures{
+		int temp;
+		ArrayList<weardness> weardnesses = new ArrayList<weardness>();
+		public temperatures(int temp){
+			this.temp = temp;
+		}
+	}
+	
+	public class weardness{
+		static int maxweardness;
+		ArrayList<String> biomeIDs = new ArrayList<String>();
+		public weardness(int weardness){
+		}
+		void addbiome(String t){
+			biomeIDs.add(t);
+		}
+	}
+	
 	void print(Object o) {
 		System.out.println(o);
 	}
@@ -27,11 +62,8 @@ public class loadbiomedata {
 		JSONArray biomeslist = biomedata.getJSONArray("biomes");
 		tempsorder = new String[11][biomeslist.length()];
 		JSONObject currentbiome = biomedata.getJSONObject(biomeslist.getString(0));
-		minheight = currentbiome.getInt("height") - 100;
 		for(int i = 0; i < biomeslist.length(); i++) {
 			currentbiome = biomedata.getJSONObject(biomeslist.getString(i));
-			checkcurrentheightmin(currentbiome);
-			checkcurrentheightmax(currentbiome);
 			for(int ix = currentbiome.getInt("temp") - currentbiome.getInt("temprange"); ix < currentbiome.getInt("temp") + currentbiome.getInt("temprange"); ix++) {
 				int iy = 0;
 				while(tempsorder[ix][iy] != null) {
@@ -40,13 +72,5 @@ public class loadbiomedata {
 				tempsorder[ix][iy] = biomeslist.getString(i);
 			}
 		}
-	}
-	void checkcurrentheightmax(JSONObject currentdata) {
-		int currentmaxheight = currentdata.getInt("height") + currentdata.getInt("maxadd");
-		maxheight = currentmaxheight > maxheight? currentmaxheight : maxheight;
-	}
-	void checkcurrentheightmin(JSONObject currentdata) {
-		int currentmaxheight = currentdata.getInt("height");
-		minheight = currentmaxheight < minheight? currentmaxheight - 100 : minheight;
 	}
 }
